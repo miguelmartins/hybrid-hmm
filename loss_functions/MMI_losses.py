1e21cbe70b300c5d2268d93d1176faa40530994f
Miguel Martins <miguelopesmartins@gmail.com>
2022-03-16 16:34:19 +0000

    Add complete likelihood loss function
    A loss function class wrapping only the complete likelihood
    objective function.

--- loss_functions/MMI_losses.py
+++ loss_functions/MMI_losses.py
@@ -184,12 +184,12 @@ class MMILoss(Loss):
         T = tf.shape(y_true)[0]
         sequence = tf.argmax(y_true, axis=1)
         emissions = tf.reduce_max(y_true * y_pred, axis=1)
-        joint = self.p_states[sequence[0]] * emissions[0]
+        joint = (self.p_states[sequence[0]] * emissions[0]) / self.p_states[sequence[0]]
         c_t = 1 / tf.reduce_sum(joint)
         joint = joint * c_t
         C_t = tf.math.log(c_t)
         for t in range(1, T):
-            joint = joint * self.trans_mat[sequence[t - 1], sequence[t]] * emissions[t]
+            joint = joint * self.trans_mat[sequence[t - 1], sequence[t]] * (emissions[t] / self.p_states[sequence[t]])
             c_t = 1 / tf.reduce_sum(joint)
             joint = joint * c_t
             C_t += tf.math.log(c_t)
@@ -197,8 +197,9 @@ class MMILoss(Loss):
 
     def _complete_likelihood_deprecated(self, y_true, y_pred):
         T = tf.shape(y_true)[0]
+        y_pred_ = y_pred / self.p_states
         sequence = tf.argmax(y_true, axis=1)
-        emissions = tf.reduce_sum(tf.math.log(tf.reduce_max(y_true * y_pred, axis=1) + 1e-12))
+        emissions = tf.reduce_sum(tf.math.log(tf.reduce_max(y_true * y_pred_, axis=1) + 1e-12))
         transitions = tf.math.log(self.p_states[sequence[0]] + 1e-12)
         for t in range(1, T):
             transitions += tf.math.log(self.trans_mat[sequence[t - 1], sequence[t]] + 1e-12)
@@ -225,18 +226,43 @@ class MMILoss(Loss):
             The likelihood P(O) given the markov chain and neural network output
         """
         T = tf.shape(y_pred)[0]
-        alpha = self.p_states * y_pred[0, :]
+        y_pred_ = y_pred / self.p_states
+        alpha = self.p_states * y_pred_[0, :]
         c_t = 1 / tf.reduce_sum(alpha)
         alpha *= c_t
         C = tf.math.log(c_t)
         for t in range(1, T):
             alpha_stay = alpha * tf.linalg.diag_part(self.trans_mat)
             alpha_go = tf.roll(alpha, shift=1, axis=0) * tf.linalg.diag_part(tf.roll(self.trans_mat, shift=1, axis=0))
-            alpha = (alpha_stay + alpha_go) * y_pred[t, :]
+            alpha = (alpha_stay + alpha_go) * y_pred_[t, :]
             c_t = 1 / tf.reduce_sum(alpha)
             alpha *= c_t
             C += tf.math.log(c_t)
         return -C
+
+
+class CompleteLikelihoodLoss(MMILoss):
+    def __init__(self, trans_mat, p_states):
+        super().__init__(trans_mat, p_states)
+
+    def call(self, y_true, y_pred):
+        """
+        Computes -(P(O, S)) for minimization under gradient descent
+
+        Parameters
+        ----------
+        y_true : tf.Tensor
+            The tensor of one-hot-encoded ground-truth
+        y_pred : tf.Tensor
+            The tensor of the output layer of a DNN
+
+        Returns
+        -------
+        tf.Variable
+            The negative complete likelihood value for optimization
+        """
+        log_complete_likelihood = self._complete_likelihood(y_true, y_pred)
+        return -log_complete_likelihood
 
 
 class ForwardLoss(Loss):
