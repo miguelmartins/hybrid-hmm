7829929effc4a5d3f1da89a7a9e00704359e0ee4
Miguel Martins <miguelopesmartins@gmail.com>
2022-07-25 16:36:24 +0100

    Add inital tests for tp and fp calculation
    Trivial unit tests for tp and fp calculation

--- tests/test_schmidt_metrics.py
+++ tests/test_schmidt_metrics.py
@@ -7,11 +7,11 @@ from utility_functions.metrics import get_segments, get_centers, get_schmidt_tp_
 
 
 @pytest.mark.tm
-class TestCanonicalSimplex(unittest.TestCase):
+class TestSchmidtMetrics(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_segments(self):
+    def test_get_segments(self):
         """
         Check if segment boundaries are correct
         """
@@ -54,3 +54,82 @@ class TestCanonicalSimplex(unittest.TestCase):
                              [60, 119, 3],
                              [120, 120, 0]])
         npt.assert_array_equal(expected, get_segments(test4))
+
+    def test_get_centers(self):
+        """
+        Test interface to show get center of windows
+        """
+
+        # Single state duration
+        test1 = np.array([0] +
+                         [1] * 3)
+        expected = np.array([[0.0, 0],
+                             [2., 1]])
+        segments = get_segments(test1)
+        np.testing.assert_array_equal(expected, get_centers(segments))
+
+        # Trivial case all even
+        test2 = np.array([0] * 2 +
+                         [1] * 2 +
+                         [2] * 2)
+        segments = get_segments(test2)
+        expected = np.array([[0.5, 0],
+                             [2.5, 1],
+                             [4.5, 2]])
+        np.testing.assert_array_equal(expected, get_centers(segments))
+
+        # Single state beginning + trivial case even
+        test3 = np.array([3] +
+                         [0] * 2 +
+                         [1] * 2 +
+                         [2] * 2)
+        segments = get_segments(test3)
+        expected = np.array([[0, 3],
+                             [1.5, 0],
+                             [3.5, 1],
+                             [5.5, 2]])
+        np.testing.assert_array_equal(expected, get_centers(segments))
+
+        # Single state middle + trivial case
+        test4 = np.array([0] * 2 +
+                         [1] * 2 +
+                         [3] +
+                         [2] * 2)
+        segments = get_segments(test4)
+        expected = np.array([[0.5, 0],
+                             [2.5, 1],
+                             [4, 3],
+                             [5.5, 2]])
+        np.testing.assert_array_equal(expected, get_centers(segments))
+
+        # Odd durations
+        test5 = np.array([0] * 3 +
+                         [1] * 3 +
+                         [2] * 3)
+        segments = get_segments(test5)
+        expected = np.array([[1, 0],
+                             [4, 1],
+                             [7., 2]])
+        np.testing.assert_array_equal(expected, get_centers(segments))
+
+    def test_schmidt_tp_fp(self):
+        """
+        Test tp, fp and total calculation for a set of examples and possible corner cases
+        """
+
+        # Perfect S+ and PPV for simple sound
+        y_true = np.array([0] * 100 +
+                           [2] * 100)
+        y_pred = y_true
+        exp_tp, exp_fp, exp_total = 2, 0, 2
+        tp, fp, total = get_schmidt_tp_fp(y_true, y_pred)
+        assert (exp_tp == tp) and (exp_fp == fp) and (exp_total == total)
+
+        # Worst case S+ and PPV for simple sound
+        y_true = np.array([0] * 100 +
+                          [2] * 100)
+        y_pred = np.array([2] * 100 +
+                          [0] * 100)
+        exp_tp, exp_fp, exp_total = 0, 2, 2
+        tp, fp, total = get_schmidt_tp_fp(y_true, y_pred)
+        assert (exp_tp == tp) and (exp_fp == fp) and (exp_total == total)
