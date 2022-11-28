08cddc570d9eb85f266fad0c95022d253aeccb02
Miguel Martins <miguelopesmartins@gmail.com>
2022-11-28 15:57:28 +0000

    Add paired t-test utility functions

---
+++ utility_functions/statistic_analysis.py
@@ -0,0 +1,21 @@
+from typing import Dict, List
+import scipy.stats as stats
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def paired_t_test_statistics(samples: Dict[str, List], alpha=0.05):
+    # all possible pairings
+    n = len(samples)
+    pairings = tuple(prod for prod in itertools.product(samples, samples))
+    name_pairs = [(pair[0][0], pair[1][0]) for pair in pairings]
+    paired_t_test = [stats.ttest_rel(pair[0][1], pair[1][1]) for pair in pairings]
+    matrix = [1 if (p_v := pair.pvalue) <= alpha else 0 for pair in paired_t_test]
+    return name_pairs, paired_t_test, np.array(matrix).reshape((n, n))
+
+def plot_t_test_matrix(mat):
+    mat_ = mat.copy().astype(np.float32)
+    for i in range(mat.shape[0]):
+        mat_[i, i] = 0.5
+    plt.imshow(mat_, cmap='gray', vmin=0, vmax=1)
